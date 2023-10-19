5fb4ac0286492313361d56660fe472ad09fde6d6
ealeksandrov7 <evlogi@weichain.tech>
2023-10-19 16:27:34 +0300

    misc: cleanup

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,16 +1,15 @@
 import { Code2, Info } from '@tamagui/lucide-icons';
 import { Link, Tabs } from 'expo-router';
+import { useCallback } from 'react';
 import { Button, Stack, useTheme } from 'tamagui';
 
 import { useToggleStorybook } from '@/hooks/useToggleStorybook';
 
-// const storybookEnabled = process.env.EXPO_PUBLIC_STORYBOOK_ENABLED === 'true';
-
-function TabLayout() {
+export default function TabLayout() {
   const tintColor = useTheme().blue8.val;
   const { isStorybookEnabled } = useToggleStorybook();
 
-  const renderStorybook = () => {
+  const renderStorybook = useCallback(() => {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const StorybookUI = require('../../../.storybook').default;
 
@@ -19,7 +18,7 @@ function TabLayout() {
         <StorybookUI />
       </Stack>
     );
-  };
+  }, [isStorybookEnabled]);
 
   return isStorybookEnabled ? (
     <Tabs
@@ -51,18 +50,3 @@ function TabLayout() {
     renderStorybook()
   );
 }
-
-const EntryPoint = TabLayout;
-
-// if (storybookEnabled) {
-//   const StorybookUI = require('../../../.storybook').default;
-//   EntryPoint = () => {
-//     return (
-//       <Stack style={{ flex: 1 }}>
-//         <StorybookUI />
-//       </Stack>
-//     );
-//   };
-// }
-
-export default EntryPoint;
